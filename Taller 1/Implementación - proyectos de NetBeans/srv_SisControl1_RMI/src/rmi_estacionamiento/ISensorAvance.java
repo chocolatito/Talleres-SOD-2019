3cf0a503e6128a4227c72e29fe5745b5ff39220c
chocolatito <sal_defrutas@hotmail.com>
2020-01-13 13:57:38 -0300

    actualizado

--- Taller 1/Implementación - proyectos de NetBeans/srv_SisControl1_RMI/src/rmi_estacionamiento/ISensorAvance.java
+++
@@ -1,8 +0,0 @@
-package rmi_estacionamiento;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ISensorAvance extends Remote {
-	public void Suscribirse(IClienteSensorAvance cliente) throws RemoteException;
-}
--- Taller 1/Implementación - proyectos de NetBeans/srv_SisControl1_RMI/src/rmi_estacionamiento/ISensorTicket.java
+++
@@ -1,8 +0,0 @@
-package rmi_estacionamiento;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ISensorTicket extends Remote {
-	public void Suscribirse(IClienteSensorTicket cliente) throws RemoteException;
-}
